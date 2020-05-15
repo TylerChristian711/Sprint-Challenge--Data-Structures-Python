b8c847f1acd6c599b746fcdcfc9587c2cc3d38a1
Tyler Christian <tylerc71197@gmail.com>
2020-05-15 12:25:52 -0400

    ring_buffer test all pass added new file of doubdlyLinkedList

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,28 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = DoublyLinkedList()
+        self.current_node = None
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current_node = self.storage.head
+        else:
+            if not self.current_node.next:
+                self.current_node.value = item
+                self.current_node = self.storage.head
+            else:
+                self.current_node.value = item
+                self.current_node = self.current_node.next
 
     def get(self):
-        pass
+        arr = []
+        current = self.storage.head
+        while current:
+            arr.append(current.value)
+            current = current.next
+        return arr
